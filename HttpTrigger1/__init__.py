0b4b3850e4a8bb79bfcc17336f0d26109c09b7a3
Luc Halmshaw <luc.halmshaw@grabx.ch>
2022-09-05 17:21:52 +0200

    added first date of the month

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,9 +1,9 @@
+import datetime 
 import logging
-
 import azure.functions as func
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -15,8 +15,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
+    todayDate = datetime.date.today()
+    if todayDate.day > 25:
+        todayDate += datetime.timedelta(7)
+    
+    print(todayDate.replace(day=1))
+
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        #msg.set(name)
+
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -2,23 +2,26 @@ import logging
 
 import azure.functions as func
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
-    if not name:
+    player= req.params.get('player')
+
+    if not name or player:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
             name = req_body.get('name')
+            player= req.params.get('player')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if name and player:
+        return func.HttpResponse(f"Hello, {name} {player}. This HTTP triggered function executed successfully.")
+    
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a name and a player in the query string or in the request body for a personalized response.",
              status_code=200
         )
