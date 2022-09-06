252c9bd81cc0e90c44410e36951ca38c78c01510
Luc Halmshaw <luc.halmshaw@grabx.ch>
2022-09-06 09:25:32 +0200

    changes

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,4 +1,5 @@
 import datetime 
+import calendar
 import logging
 import azure.functions as func
 
@@ -16,20 +17,24 @@ def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
         else:
             name = req_body.get('name')
     
-    todayDate = datetime.date.today()
-    if todayDate.day > 25:
-        todayDate += datetime.timedelta(7)
+    # todayDate = datetime.date.today()
+    # if todayDate.day > 25:
+    #     todayDate += datetime.timedelta(7)
 
-def last_day_of_month(d: datetime.date) -> datetime.date:
-    return (
-        datetime.date(d.year + d.month//12, d.month % 12 + 1, 1) -
-        datetime.timedelta(days=1)
+    # def last_day_of_month(d: datetime.date) -> datetime.date:
+    #     return (
+    #         datetime.date(d.year + d.month//12, d.month % 12 + 1, 1) -
+    #         datetime.timedelta(days=1)
+    #     )
+
+    current_date = datetime.date.today()
+    first_day_of_month = datetime.date(current_date.year, current_date.month, 1)
+    last_day_of_month = datetime.date(current_date.year, current_date.month, calendar.monthrange(current_date.year, current_date.month)[1])
  
-    print(todayDate.replace(day=1))
-    print(datetime.timedelta(days=1))
+    print(first_day_of_month)
+    print(last_day_of_month)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.{first_day_of_month, last_day_of_month}")
         #msg.set(name)
 
     else:
