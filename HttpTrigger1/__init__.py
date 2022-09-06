468270738b927dfad5ee881e8a48855e8976d6a3
Luc Halmshaw <luc.halmshaw@grabx.ch>
2022-09-06 08:26:40 +0200

    trying to add last day of the month

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -19,9 +19,10 @@ def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
     todayDate = datetime.date.today()
     if todayDate.day > 25:
         todayDate += datetime.timedelta(7)
+    lastDayOfMonth = datetime.date(currentDate.year, currentDate.month, 1)
     
     print(todayDate.replace(day=1))
-
+    print(lastDayOfMonth)
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
         #msg.set(name)
@@ -31,9 +32,3 @@ def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
-
-import date.today 
-date.today()
-datetime.date(2017, 1, 3)
-date.today().replace(day=31)
-datetime.date(2017, 1, 31)
