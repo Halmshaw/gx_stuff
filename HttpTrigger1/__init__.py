537a653451b0540a212fc1cd12818f9a49019531
Luc Halmshaw <luc.halmshaw@grabx.ch>
2022-09-07 09:26:03 +0200

    adding notes to the program

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,22 +1,22 @@
-import datetime 
-import calendar
-import logging
-import json
-import azure.functions as func
+import datetime #imports the module of date and time into python 
+import calendar #Imports the calendar data into python 
+import logging #is used to log messages you want to be able to see
+import json #converts python dictionary into a Json string
+import azure.functions as func # Allows the code to connect to the Azure cloud
 
 
 
-def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
+def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str: #def is used to define the main function, 
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
+    name = req.params.get('name') #turns req.params.get into a variable 
+    if not name: #it checks if the statement is true or not 
+        try: 
             req_body = req.get_json()
         except ValueError:
             pass
-        else:
-            name = req_body.get('name')
+        else:  
+            name = req_body.get('name') #if the statement is correct the name will be displayed 
     
     # todayDate = datetime.date.today()
     # if todayDate.day > 25:
@@ -28,12 +28,12 @@ def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
     #         datetime.timedelta(days=1)
     #     )
            
-    current_date = datetime.date.today()
-    first_day_of_month = datetime.date(current_date.year, current_date.month, 1)
+    current_date = datetime.date.today() #turned the value into an variable 
+    first_day_of_month = datetime.date(current_date.year, current_date.month, 1) #
     last_day_of_month = datetime.date(current_date.year, current_date.month, calendar.monthrange(current_date.year, current_date.month)[1])
  
-    strFirst = first_day_of_month.strftime("%d/%m/%Y")
-    strLast = last_day_of_month.strftime("%d/%m/%Y")
+    strFirst = first_day_of_month.strftime("%d/%m/%Y") # turns the data into a variable
+    strLast = last_day_of_month.strftime("%d/%m/%Y")   # turns the data into a variable
     value =  {
         "firstDateOfMonth": strFirst,
         "lastDateOfMonth": strLast
@@ -42,12 +42,12 @@ def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
 
 
 
-    print(json.dumps(value)) 
+    print(json.dumps(value)) #prints the first and last day of the month 
 
     # print(first_day_of_month)
     # print(last_day_of_month)
     
-    return func.HttpResponse( json.dumps(value))
+    return func.HttpResponse( json.dumps(value)) #this sends the functions results back to the programmer 
         #msg.set(name)
     
 
---
+++ string/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    string = req.params.get('string')
+    if not string:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            string = req_body.get('string')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
